4d5ff60a09073e7a7b09a0866f7554d29c532607
ZhangSir <57878778@qq.com>
2023-12-17 15:36:26 +0800

    feat: updated badge component

--- src/components/badge/badge.ts
+++ src/components/badge/badge.ts
@@ -1,21 +1,21 @@
 import type { ExtractPropTypes } from "vue";
 
 export const BadgeType = {
-    primary: 'primary'
-}
+  primary: "primary",
+};
 
 export const badgeProps = {
-    text: {
-        type: String,
-        default: ''
-    },
-    color: {
-        type: String,
-        default: '',
-    },
-    status: {
-        type: String,
-        default: BadgeType.primary
-    }
-}
-export type BadgeProps = ExtractPropTypes<typeof badgeProps>
+  text: {
+    type: String,
+    default: "",
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    default: BadgeType.primary,
+  },
+};
+export type BadgeProps = ExtractPropTypes<typeof badgeProps>;
--- src/components/badge/badge.vue
+++ src/components/badge/badge.vue
@@ -1,14 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <span 
-        v-if="props.status == BadgeType.primary"
-        class="px-[0.3125rem] py-[0.125rem] ml-1 text-[#fff] text-[0.7rem] rounded-[5px]"
-        :style="`background-color: ${props.color ? props.color : '#6670eb'}`">
-            {{ props.text }}
-    </span>
+  <span
+    v-if="props.status == BadgeType.primary"
+    class="px-[0.3125rem] py-[0.125rem] ml-1 text-[#fff] text-[0.7rem] rounded-[5px]"
+    :style="`background-color: ${props.color ? props.color : '#6670eb'}`"
+  >
+    {{ props.text }}
+  </span>
 </template>
 <script setup lang="ts">
-import { BadgeType, badgeProps } from './badge';
+import { BadgeType, badgeProps } from "./badge";
 
 const props = defineProps(badgeProps);
 </script>
